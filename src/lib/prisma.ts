077f807d2a4fa61518e4dfc2871b5f810e022a4e
Dyvinitos <105842249+Dyvinitos@users.noreply.github.com>
2025-10-27 01:07:50 +0100

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from "@/app/generated/prisma-client";
+// src/lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["error", "warn"], // tu peux mettre ["query"] si tu veux voir toutes les requêtes
-  });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
